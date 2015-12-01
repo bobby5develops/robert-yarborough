14ed02381ded106098a71256839420f5d38cda48
robert yarborough <robert-yarborough@roberts-MacBook-Pro.local>
2015-11-30 22:19:56 -0500

    routing re-configured

--- app/app.js
+++ app/app.js
@@ -5,12 +5,39 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.view3',
-  'myApp.view4',
-  'myApp.view5',
-  'myApp.view6',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+      //route for view1
+      .when('/view1',{
+          templateUrl: 'view1/view1.html',
+          controller: 'View1Ctrl'
+      })
+      //route for view2
+      .when('/view2',{
+          templateUrl: 'view2/view2.html',
+          controller: 'View2Ctrl'
+      })
+      //route for view3
+      .when('/view3',{
+          templateUrl: 'view3/view3.html',
+          controller: 'View3Ctrl'
+      })
+      .otherwise({redirectTo: '/view1'});
+
 }]);
+
+// create the controller and inject Angular's $scope
+myApp.controller('View1Ctrl', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+myApp.controller('View2Ctrl', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+myApp.controller('View3Ctrl', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
--- app/view3/view3.js
+++ app/view3/view3.js
@@ -1,7 +1,6 @@
 'use strict';
 
 angular.module('myApp.view3', ['ngRoute'])
-
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view3', {
             templateUrl: 'view3/view3.html',
